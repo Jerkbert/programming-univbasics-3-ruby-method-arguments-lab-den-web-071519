3e7d48357ceb4bb2695a6b428d24308c314cee9e
Rob Ballantyne <raballantyne@gmail.com>
2019-06-27 17:27:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,18 +2,18 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-indroduction(name)
+#indroduction(name)
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(name, language)
+#introduction_with_language(name, language)
 
 def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
   
 end 
 
-introduction_with_language_optional(name, language)
+#introduction_with_language_optional(name, language)
 
